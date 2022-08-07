520ce207d5aace1a4e2cabbeb079d0faaaaa454f
Hisashi Iwamoto <hisasi622@gmail.com>
2022-08-07 12:03:15 +0900

    [Update ]line botでメッセージを送信

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const request = require('request')
 const line = require('@line/bot-sdk')
+const axios = require('axios')
 
 const client = new line.Client({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
@@ -10,34 +11,40 @@ const userId = process.env.USER_ID
 
 const message = {
   type: 'text',
-  text: 'Hello World!',
+  text: 'Hello World',
 }
 
-// client
-//   .pushMessage(userId, getQiitaData)
-//   .then(() => {
-//     console.log('success')
-//   })
-//   .catch((err) => {
-//     console.log(err)
-//   })
+function sendMessage(message) {
+  client
+    .pushMessage(userId, message)
+    .then(() => {
+      console.log('success')
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
 
-const getQiitaData = () => {
+function sendBot(data) {
+  const message = data
+  const sendMessages = []
+  for (let i = 0; i < 4; i++) {
+    sendMessages.push({
+      type: 'text',
+      text: message[i].node.title + '\n' + message[i].node.linkUrl,
+    })
+  }
+  sendMessage(sendMessages)
+}
+
+async function getQiita() {
   const URL = 'https://qiita-api.vercel.app/api/trend'
-  request.get(
-    {
-      uri: URL,
-      headers: { 'Content-type': 'application/json' },
-      json: true,
-    },
-    (err, res, body) => {
-      if (err) {
-        console.log(err)
-      } else {
-        console.log(body)
-      }
-    }
-  )
+  try {
+    const response = await axios.get(URL)
+    sendBot(response.data)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-console.log(getQiitaData)
+getQiita()
